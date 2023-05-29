392e529ec4a5a2a1ef4c4fec585a28eaed8995a4
hung <hunglt.ee@gmail.com>
2023-05-29 16:24:08 +0700

    new sheet from template

--- app/services/google_sheets_service.rb
+++ app/services/google_sheets_service.rb
@@ -15,39 +15,41 @@ class GoogleSheetsService
     spreadsheet_id = ENV['SPREADSHEETS_ID'] #not worksheet S
     spreadsheet = @service.get_spreadsheet(spreadsheet_id)
 
-    # HÀNH ĐỘNG 1: STILL INSERT NEW SHEET => NOT RENAME LAST SHEET
-    rewrite_sheet = spreadsheet.sheets.last
-    rewrite_sheet_id = rewrite_sheet.instance_variable_get('@properties').instance_variable_get('@sheet_id') #ENV['TEMPLATE_ID'] ?? original ??
-    # spreadsheet_properties = spreadsheet.sheets.find { |sheet| sheet.properties.sheet_id == rewrite_sheet_id.to_i }.properties
+    # HÀNH ĐỘNG 1: NEW SHEET => FROM TEMPLATE SHEET: copy data, format ?
+    template_sheet_id = ENV['TEMPLATE_SHEET_ID']
+    # spreadsheet_properties = spreadsheet.sheets.find { |sheet| sheet.properties.sheet_id == template_sheet_id.to_i }.properties
 
-    rewrite_sheet_name = Time.now.strftime('%d/%m/%Y %H:%M')
+    new_sheet_name = Time.now.strftime('%d/%m/%Y %H:%M')
     requests = [
       {
         duplicate_sheet: {
-          source_sheet_id: rewrite_sheet_id,
-          new_sheet_name: rewrite_sheet_name
-          # insert_sheet_index: spreadsheet.sheets.count + 1
+          source_sheet_id: template_sheet_id,
+          new_sheet_name: new_sheet_name,
+          insert_sheet_index: spreadsheet.sheets.count + 1
         }
       }
     ]
     @service.batch_update_spreadsheet(spreadsheet_id, Google::Apis::SheetsV4::BatchUpdateSpreadsheetRequest.new(requests: requests))
 
-    debugger
-    # HÀNH ĐỘNG 2
+    # debugger
+    # HÀNH ĐỘNG 2: SOME ACTION BY SHEET ID
     spreadsheet = @service.get_spreadsheet(spreadsheet_id)
     new_sheet_id = nil
+
+    # let's find new_sheet.id
     spreadsheet.sheets.each do |sheet|
-      if sheet.properties.title == rewrite_sheet_name
+      if sheet.properties.title == new_sheet_name
         new_sheet_id = sheet.properties.sheet_id
         break
       end
     end
     return if new_sheet_id.blank?
+
     requests = [
       {
         copy_paste: {
           source: {
-            sheet_id: rewrite_sheet_id,
+            sheet_id: template_sheet_id,
           },
           destination: {
             sheet_id: new_sheet_id
@@ -59,11 +61,11 @@ class GoogleSheetsService
     ]
     @service.batch_update_spreadsheet(spreadsheet_id, Google::Apis::SheetsV4::BatchUpdateSpreadsheetRequest.new(requests: requests))
 
-    debugger
-    # HÀNH ĐỘNG 3
+    # debugger
+    # HÀNH ĐỘNG 3: XÓA DỮ LIỆU TRONG KHOẢNG A1:L1000 <- ONLY BY SHEET NAME
     spreadsheet = @service.get_spreadsheet(spreadsheet_id)
     clear_request_body = Google::Apis::SheetsV4::ClearValuesRequest.new
-    @service.clear_values(spreadsheet_id, "#{rewrite_sheet_name}!A1:L1000", clear_request_body)
+    @service.clear_values(spreadsheet_id, "#{new_sheet_name}!A1:L1000", clear_request_body)
 
   end
 end
